2bfaef452ebb0a49bdc26b753d439fb7ebc4a2a4
Avishek-kr <ak251997@gmail.com>
2024-12-30 13:32:58 +0530

    folder created

--- src/components/main-header/Nav-link.js
+++
@@ -1,9 +0,0 @@
-'use client';
-import Link from "next/link";
-import classes from './Nav-link.module.css'
-import { usePathname } from "next/navigation";
-
-export default function NavLink({ href, children }) {
-    const params = usePathname();
-    return <Link className={params.startsWith(href) ? classes.active : undefined} href={href}>{children}</Link>
-}
